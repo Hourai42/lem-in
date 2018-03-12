25fd31788d836d82eba3010456defe4f2d7a52a8
Ted <marixalijustice@gmail.com>
2018-03-12 00:10:26 -0700

    k

--- srcs/links.c
+++ srcs/links.c
@@ -12,13 +12,28 @@
 
 #include "lemin.h"
 
+/*
+** ft_split by "-" and ensure that index[0] is first name, index[1] is second name, and index[2] is 0.
+** Make sure that both are valid room names.
+** Are repeat links allowed? I guess-- there can be as many links between two rooms as necessary. 
+** Even if pointless... I guess. Unless there's more than one link from start to end, meaning you can put more
+** than one ant in at a time. Which makes sense-- no need to check for link repeats! 
+** Basically not much error checking involved.
+*/
+
+int	setup_link(t_super *hold, char *line)
+{
+	// Kk.
+}
+
 int	validate_link(t_super *hold, char *line)
 {
 	if (line[0] == '#' && line[1] != '#')
 		return (0);
 	if (line[0] == '#' && line[1] == '#')
 		return (INVALID_LINKS);
-	// Think about how you're going to validate the links.
+	return (setup_link(hold, line));
+
 }
 
 int	set_links(t_super *hold, **line)
