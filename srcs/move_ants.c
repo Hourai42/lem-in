bd82a22b5f6bbc1c981e7ce1261211bf5e619f8a
Ted <marixalijustice@gmail.com>
2018-03-14 01:27:40 -0700

    First algo push

--- srcs/move_ants.c
+++ srcs/move_ants.c
@@ -12,9 +12,67 @@
 
 #include "lemin.h"
 
+void	end_movement(t_ant ant, t_graph *ptr, int ant_nbr)
+{
+	if (ptr != NULL)
+	{
+		ptr->occupied = 1;
+		ant.in_here = ptr;
+		if (ptr->end == 1)
+			ant.end = 1;
+		printf("L%d-%s ", ant_nbr, ptr->room_name);
+	}
+}
+
+/*
+** Steps is the only changing criteria.
+*/
+
+int		choose_movement(t_ant ant, int ant_nbr)
+{
+	t_graph *ptr;
+	int steps;
+	t_link *mover;
+
+	mover = ant.in_here->link;
+	steps = ant.in_here->steps;
+	ptr = NULL;
+	while (mover != NULL)
+	{
+		if (mover->connected_to->steps < steps && 
+		mover->connected_to->occupied == 0 && 
+		mover->connected_to->successful_path == 1)
+		{
+			ptr = mover->connected_to;
+			steps = mover->connected_to->steps;
+		}
+		mover = mover->next;
+	}	
+	if (ptr == NULL)
+		return (0);
+	end_movement(ant, ptr, ant_nbr);
+	return (1);
+}
+
+void	move(t_ant ant, int ant_nbr)
+{
+	t_graph *ptr;
+
+	ptr = ant.in_here;
+	if (choose_movement(ant, ant_nbr) == 1)
+		ptr->occupied = 0;
+}
+
 void	one_turn(t_super *hold)
 {
+	int i;
 
+	i = -1;
+	while (++i < hold->ant_total)
+	{
+		if (hold->farm[1].end != 1)
+			move(hold->farm[1], i + 1);
+	}
 }
 
 int	ants_in_end(t_super *hold)
@@ -24,26 +82,27 @@ int	ants_in_end(t_super *hold)
 
 	ants_end = 0;
 	i = -1;
-	while (++i < ants_end)
+	while (++i < hold->ant_total)
 	{
 		if (hold->farm[i].end == 1)
 			ants_end++;
 	}
+	return (ants_end);
 }
 
 /*
 ** This function moves the ants until they are all in end.
 ** 1 movement per ant per turn.
 ** Add the waiting heuristic w/ steps later.
-** L(ant-number)-(Room Name)
 */
 
 void	move_ants(t_super *hold)
 {
 	while (42)
 	{
-		one_turn(t_super *hold)
+		one_turn(hold);
 		if (ants_in_end(hold) == hold->ant_total)
-			break;	
+			break;
+		ft_printf("\n");
 	}
 }
